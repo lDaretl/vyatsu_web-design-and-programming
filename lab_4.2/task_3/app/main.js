cc0f5c512150847bfc0b2a5e99d17c5d965ae3c0
Artyom Anufriev <daret@mr.artoym@mail.ru>
2025-10-11 17:26:22 +0300

    complete - lab_4.2

---
+++ lab_4.2/task_3/app/main.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+            inStock: false,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+                }
+            ],
+            cart: 0
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+    }
+}).mount('#app');
---
+++ lab_4.2/task_4/app/main.js
@@ -0,0 +1,112 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            premium: true
+        };
+    }
+});
+
+app.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+    <ul>
+      <li v-for="detail in details" :key="detail">{{ detail }}</li>
+    </ul>
+  `
+});
+
+app.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+    <div class="product">
+      <div class="product-image">
+        <img :src="image" alt="product image">
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+
+        <product-details :details="details"></product-details>
+
+        <div
+          class="color-box"
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+        ></div>
+
+        <button
+          @click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+        >
+          Add to cart
+        </button>
+
+        <div class="cart">
+          <p>Cart ({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+    data() {
+        return {
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [{
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+            cart: 0
+        };
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        shipping() {
+            return this.premium ? 'Free' : 2.99;
+        }
+    }
+});
+
+app.mount('#app');
